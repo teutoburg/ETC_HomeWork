3013090fb65f4be423e3bbbef755e29e29bb21dc
teutoburg <ghost@instruct.at>
2022-11-14 14:31:25 +0100

    docstrings and cleanup

--- etc.py
+++ etc.py
@@ -11,7 +11,7 @@ Created on Fri Nov 11 14:34:00 2022
 @author: teutoburg
 """
 
-__version__ = "0.1"
+__version__ = "0.2"
 
 import numpy as np
 
@@ -33,15 +33,16 @@ defaults = {"filter_name": "Ks",
             "observatory": "Paranal"}
 
 
-def _noise(target_counts, sky_counts_pp, dit):
+def _get_noise(target_counts, sky_counts_pp, dit):
     # HACK: The following factors are directly taken from the ESO HAWK-I ETC
     drs_factor = 1.1
     read_out_noise = 5 * u.electron/u.pixel  # per DIT
     dark_current = .01 * u.electron/u.pixel/u.s
 
     # HACK: The pixel number below is most likely calculated from the PSF by
-    #       official ESO HAWK-I ETC using a Image Quality FWHM of 0.6 arcsec.
-    #       It has been fixed for the scope of this project.
+    #     official ESO HAWK-I ETC using a Image Quality FWHM of 0.6 arcsec,
+    #     or a seeing of 0.8 arcsec FWHM, which is described as the 50% value.
+    #     It has been fixed for the scope of this project.
     n_pixel = 101 * u.pixel
 
     dark = dark_current * dit
@@ -52,9 +53,31 @@ def _noise(target_counts, sky_counts_pp, dit):
 
 
 def signal_to_noise(target_counts, sky_counts_pp, dit, n_dit):
-    """Formula adapted from the official ESO HAWK-I ETC."""
+    """
+    Calculate the S/N ratio for given target and sky counts and exposure time.
+
+    Formula adapted from the official ESO HAWK-I ETC.
+
+    Parameters
+    ----------
+    target_counts : astropy.Quantity
+        DESCRIPTION.
+    sky_counts_pp : astropy.Quantity
+        DESCRIPTION.
+    dit : astropy.Quantity
+        Detector integration time in seconds.
+    n_dit : int
+        Number of integrations.
+
+    Returns
+    -------
+    TYPE
+        DESCRIPTION.
+
+    """
     signal = np.sqrt(n_dit) * target_counts
-    return (signal / _noise(target_counts, sky_counts_pp, dit)).value
+    noise = _get_noise(target_counts, sky_counts_pp, dit)
+    return (signal / noise).value
 
 
 def _to_electrons(flux, dit, aperture_area):
@@ -66,31 +89,50 @@ def _to_electrons(flux, dit, aperture_area):
     return flux * dit * aperture_area * eff
 
 
-def create_sky(pixel_scale):
+def create_sky(pixel_scale, dit, **kwargs):
+    """
+    Estimate number of electrons per pixel expected from sky background.
+
+    Parameters
+    ----------
+    pixel_scale : astropy.Quantity
+        Pixel scale of the optical system in pixel per square meter.
+    dit : astropy.Quantity
+        Detector integration time in seconds.
+    **kwargs : TYPE
+        DESCRIPTION.
+
+    Returns
+    -------
+    sky_count : astropy.Quantity
+        Sky level in electrons per pixel.
+
+    """
+    # TODO: investigate if this could be sped up by a decorator storing the
+    #       output of last few calls for given parameters...
+    defaults = {"filter_name": "Ks",
+                "instrument": "HAWKI",
+                "observatory": "Paranal",
+                "airmass": 1.5, "pwv": 10.0}
     therm_dict = {"incl_therm": "Y", "therm_t1": 285.0, "therm_e1": 0.20,
                   "therm_t2": 288.0, "therm_e2": 0.10,  "therm_t3": 33.0,
                   "therm_e3": 0.01}
-
-    sky = hmbp.in_skycalc_background("Ks", instrument="HAWKI",
-                                     observatory="Paranal",
-                                     airmass=1.5, pwv=10.0,
-                                     **therm_dict)  # * u.arcsec**(-2)
+    parameters = defaults | therm_dict | kwargs
+    sky = hmbp.in_skycalc_background(**parameters)
     return sky * dit / pixel_scale * u.electron/u.photon
 
 
-dit = 60 * u.s
-n_dit = 60
+# dit = 60 * u.s
+# n_dit = 60
 
-assert dit * n_dit == 1 * u.h  # total observing time should be 1 h
+# assert dit * n_dit == 1 * u.h  # total observing time should be 1 h
 
-# conditions: https://www.eso.org/sci/facilities/paranal/astroclimate/site.html
+# aperture = 8.2 * u.m  # VLT aperture
+# aperture_area = 0.785398 * (aperture**2)  # pi/4 -> circle area
+# pixel_scale = 4*2048*2048*u.pixel / aperture_area
 
-aperture = 8.2 * u.m  # VLT aperture
-aperture_area = np.pi/4 * (aperture**2)
-pixel_scale = 4*2048*2048*u.pixel / aperture_area
-
-flux = hmbp.for_flux_in_filter(flux=22*u.mag, **defaults)
-sky = create_sky(pixel_scale)
-target_counts = _to_electrons(flux, dit, aperture_area)
-print(target_counts)
-print(signal_to_noise(target_counts, sky, dit, n_dit))
+# flux = hmbp.for_flux_in_filter(flux=22*u.mag, **defaults)
+# sky = create_sky(pixel_scale)
+# target_counts = _to_electrons(flux, dit, aperture_area)
+# print(target_counts)
+# print(signal_to_noise(target_counts, sky, dit, n_dit))
