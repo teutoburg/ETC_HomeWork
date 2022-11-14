1b5262806071a8da9e7f911617655e0a01e39734
teutoburg <ghost@instruct.at>
2022-11-14 14:42:09 +0100

    Turned into OOP frame...

--- etc.py
+++ etc.py
@@ -33,93 +33,97 @@ defaults = {"filter_name": "Ks",
             "observatory": "Paranal"}
 
 
-def _get_noise(target_counts, sky_counts_pp, dit):
-    # HACK: The following factors are directly taken from the ESO HAWK-I ETC
-    drs_factor = 1.1
-    read_out_noise = 5 * u.electron/u.pixel  # per DIT
-    dark_current = .01 * u.electron/u.pixel/u.s
+class HawkiEtc():
+    def __init__(self):
+        pass
 
-    # HACK: The pixel number below is most likely calculated from the PSF by
-    #     official ESO HAWK-I ETC using a Image Quality FWHM of 0.6 arcsec,
-    #     or a seeing of 0.8 arcsec FWHM, which is described as the 50% value.
-    #     It has been fixed for the scope of this project.
-    n_pixel = 101 * u.pixel
+    @staticmethod
+    def _get_noise(target_counts, sky_counts_pp, dit):
+        # HACK: The following factors are directly taken from ESO HAWK-I ETC
+        drs_factor = 1.1
+        read_out_noise = 5 * u.electron/u.pixel  # per DIT
+        dark_current = .01 * u.electron/u.pixel/u.s
 
-    dark = dark_current * dit
-    readout = read_out_noise.value**2 * u.electron/u.pixel
-    background = drs_factor * n_pixel * (sky_counts_pp + dark + readout)
+        # HACK: The pixel number below is most likely calculated from the PSF
+        #       by official ESO HAWK-I ETC using a Image Quality FWHM of
+        #       0.6 arcsec, or a seeing of 0.8 arcsec FWHM, which is described
+        #       as the 50% value for observing conditions. It has been fixed
+        #       for the scope of this project.
+        n_pixel = 101 * u.pixel
 
-    return np.sqrt(target_counts + background)
+        dark = dark_current * dit
+        readout = read_out_noise.value**2 * u.electron/u.pixel
+        background = drs_factor * n_pixel * (sky_counts_pp + dark + readout)
 
+        return np.sqrt(target_counts + background)
 
-def signal_to_noise(target_counts, sky_counts_pp, dit, n_dit):
-    """
-    Calculate the S/N ratio for given target and sky counts and exposure time.
+    def signal_to_noise(self, target_counts, sky_counts_pp, dit, n_dit):
+        """
+        Calculate S/N ratio for given target and sky counts and exposure time.
 
-    Formula adapted from the official ESO HAWK-I ETC.
+        Formula adapted from the official ESO HAWK-I ETC.
 
-    Parameters
-    ----------
-    target_counts : astropy.Quantity
-        DESCRIPTION.
-    sky_counts_pp : astropy.Quantity
-        DESCRIPTION.
-    dit : astropy.Quantity
-        Detector integration time in seconds.
-    n_dit : int
-        Number of integrations.
+        Parameters
+        ----------
+        target_counts : astropy.Quantity
+            DESCRIPTION.
+        sky_counts_pp : astropy.Quantity
+            DESCRIPTION.
+        dit : astropy.Quantity
+            Detector integration time in seconds.
+        n_dit : int
+            Number of integrations.
 
-    Returns
-    -------
-    TYPE
-        DESCRIPTION.
+        Returns
+        -------
+        TYPE
+            DESCRIPTION.
 
-    """
-    signal = np.sqrt(n_dit) * target_counts
-    noise = _get_noise(target_counts, sky_counts_pp, dit)
-    return (signal / noise).value
+        """
+        signal = np.sqrt(n_dit) * target_counts
+        noise = self._get_noise(target_counts, sky_counts_pp, dit)
+        return (signal / noise).value
 
+    @staticmethod
+    def _to_electrons(flux, dit, aperture_area):
+        # HACK: The following conversion factor has been derived to match the
+        #       values to those produced by the official ESO HAWK-I ETC, it
+        #       has no physical derivation! When used with AB magnitudes,
+        #       the factor is closer to 0.4...
+        eff = .3581 * u.electron / u.photon
+        return flux * dit * aperture_area * eff
 
-def _to_electrons(flux, dit, aperture_area):
-    # HACK: The following conversion factor has been derived to match the
-    #       values to those produced by the official ESO HAWK-I ETC, it has no
-    #       physical derivation! When used with AB magnitudes, the factor is
-    #       closer to 0.4...
-    eff = .3581 * u.electron / u.photon
-    return flux * dit * aperture_area * eff
+    def create_sky(self, pixel_scale, dit, **kwargs):
+        """
+        Estimate number of electrons per pixel expected from sky background.
 
+        Parameters
+        ----------
+        pixel_scale : astropy.Quantity
+            Pixel scale of the optical system in pixel per square meter.
+        dit : astropy.Quantity
+            Detector integration time in seconds.
+        **kwargs : TYPE
+            DESCRIPTION.
 
-def create_sky(pixel_scale, dit, **kwargs):
-    """
-    Estimate number of electrons per pixel expected from sky background.
+        Returns
+        -------
+        sky_count : astropy.Quantity
+            Sky level in electrons per pixel.
 
-    Parameters
-    ----------
-    pixel_scale : astropy.Quantity
-        Pixel scale of the optical system in pixel per square meter.
-    dit : astropy.Quantity
-        Detector integration time in seconds.
-    **kwargs : TYPE
-        DESCRIPTION.
-
-    Returns
-    -------
-    sky_count : astropy.Quantity
-        Sky level in electrons per pixel.
-
-    """
-    # TODO: investigate if this could be sped up by a decorator storing the
-    #       output of last few calls for given parameters...
-    defaults = {"filter_name": "Ks",
-                "instrument": "HAWKI",
-                "observatory": "Paranal",
-                "airmass": 1.5, "pwv": 10.0}
-    therm_dict = {"incl_therm": "Y", "therm_t1": 285.0, "therm_e1": 0.20,
-                  "therm_t2": 288.0, "therm_e2": 0.10,  "therm_t3": 33.0,
-                  "therm_e3": 0.01}
-    parameters = defaults | therm_dict | kwargs
-    sky = hmbp.in_skycalc_background(**parameters)
-    return sky * dit / pixel_scale * u.electron/u.photon
+        """
+        # TODO: investigate if this could be sped up by a decorator storing the
+        #       output of last few calls for given parameters...
+        defaults = {"filter_name": "Ks",
+                    "instrument": "HAWKI",
+                    "observatory": "Paranal",
+                    "airmass": 1.5, "pwv": 10.0}
+        therm_dict = {"incl_therm": "Y", "therm_t1": 285.0, "therm_e1": 0.20,
+                      "therm_t2": 288.0, "therm_e2": 0.10,  "therm_t3": 33.0,
+                      "therm_e3": 0.01}
+        parameters = defaults | therm_dict | kwargs
+        sky = hmbp.in_skycalc_background(**parameters)
+        return sky * dit / pixel_scale * u.electron/u.photon
 
 
 # dit = 60 * u.s
