8ae10e2a7b0820a8c758a011cb53d7b37dbbb30a
teutoburg <ghost@instruct.at>
2022-11-15 14:25:25 +0100

    mostly docstring changes...

--- etc.py
+++ etc.py
@@ -13,7 +13,7 @@ Created on Fri Nov 11 14:34:00 2022
 @author: teutoburg
 """
 
-__version__ = "0.3"
+__version__ = "0.4"
 
 from functools import lru_cache
 
@@ -35,11 +35,65 @@ __status__ = "Prototype"
 
 
 class HawkiEtc():
+    """
+    Simplified Exposure Time Calculator class for the ESO HAWK-I instrument.
+
+    Two main functions are currently implemented as methods of this class (see
+    also below for example usage), ``sn_for_mag`` and ``mag_for_sn``, which
+    will estimate signal-to-noise ratio for a given magnitude, or the limiting
+    magnitude for a given signal-to-noise ratio respectively.
+
+    The constructor for this class currently does not any parameters.
+
+    All values which are not dimensionless make heavy use of ``astropy`` units.
+    This is documented in all public methods.
+
+
+    Limitations
+    -----------
+    Currently, only the K(s) band is tested, and used as a default value for
+    ``filter_name``, along with the arguments ``"instrument" = "HAWKI"`` and
+    ``"observatory" = "Paranal"``. These values can be accessed via the class
+    attribute ``HawkiEtc.default_kwargs``, but behavior with other values is
+    not tested. Similarly, the aperture diameter and detector pixel amount are
+    fixed to the HAWK-I instrument and the VLT telescope.
+
+    Other current limitations are: the conversion from photons to electrons
+    is derived from the official ESO HAWK-I ETC, and lacks a more physical
+    formula; the amount of pixels used for noise estimations is fixed to that
+    corresponding to a seeing of 0.8 arcsec, which is described as the 50 %
+    value of observing conditions for the site.
+
+    Examples
+    --------
+    Setup instance and exposure time:
+        >>> from astropy import units as u
+        >>> from etc import HawkiEtc
+        >>>
+        >>> dit = 60 * u.s  # in seconds, needs Quantity
+        >>> n_dit = 60  # simple integer
+        >>> # total observing time in this example should be 1 h:
+        >>> assert dit * n_dit == 1 * u.h
+        >>>
+        >>> calc = HawkiEtc()  # create ETC instance
+
+    Calculate S/N ratio from given magnitude:
+        >>> lim_mag = 21*u.mag  # only Vega mags currently fully supported!
+        >>> sn_ratio = calc.sn_for_mag(lim_mag, dit, n_dit)
+        >>> print(f"S/N: {sn_ratio:.2f}")  # formatted printing
+
+    Calculate limiting magnitude from given S/N ratio:
+        >>> sn_ratio = 5.0  # dimensionless
+        >>> lim_mag = calc.mag_for_sn(sn_ratio, dit, n_dit)
+        >>> print(f"{lim_mag:.2f}")  # formatted printing
+
+    """
+
     default_kwargs = {"filter_name": "Ks",
                       "instrument": "HAWKI",
                       "observatory": "Paranal"}
     aperture = aperture = 8.2 * u.m  # VLT aperture
-    pixels = 4*2048*2048 * u.pixel  # HAWK-I detector pixel amount
+    pixels = 4*2048*2048 * u.pixel  # HAWK-I detector pixel amount (simplified)
 
     @property
     def aperture_area(self) -> Quantity[u.m**2]:
@@ -138,7 +192,8 @@ class HawkiEtc():
         dit : astropy.Quantity
             Detector integration time in seconds.
         **kwargs : dict
-            DESCRIPTION.
+            Any keyword arguments taken by ``hmbp.in_skycalc_background``, e.g.
+            `airmass` or `pwv`.
 
         Raises
         ------
@@ -167,9 +222,32 @@ class HawkiEtc():
 
     def sn_for_mag(self, mag: Quantity[u.mag],
                    dit: Quantity[u.s], n_dit: int) -> float:
+        """
+        Calculate S/N ratio from given magnitude and integration time.
+
+        Magnitude input `mag` is assumed to be Vega magnitudes. Float input
+        will be converted to mag. AB magnitudes are currently not tested.
+
+        Parameters
+        ----------
+        mag : astropy.Quantity
+            Input (Vega) magnitude.
+        dit : astropy.Quantity
+            Detector integration time in seconds, must be >= 10 s for non-
+            destructive reading (NDR) as per the official ESO HAWK-I
+            instrument description.
+        n_dit : int
+            Number of integrations, must be >= 1.
+
+        Returns
+        -------
+        s_n : float
+            Signal-to-noise ratio (dimensionless).
+
+        """
         mag = mag << u.mag
         sky_counts_pp = self.create_sky(dit)
-        flux = hmbp.for_flux_in_filter(flux=mag, **calc.default_kwargs)
+        flux = hmbp.for_flux_in_filter(flux=mag, **self.default_kwargs)
         target_counts = self._to_electrons(flux, dit)
         s_n = self.signal_to_noise(target_counts, sky_counts_pp, dit, n_dit)
         return s_n
@@ -181,7 +259,7 @@ class HawkiEtc():
         mag = mag[0]
         with HiddenPrints():
             sky_counts_pp = self.create_sky(dit)
-        flux = hmbp.for_flux_in_filter(flux=mag, **calc.default_kwargs)
+        flux = hmbp.for_flux_in_filter(flux=mag, **self.default_kwargs)
         target_counts = self._to_electrons(flux, dit)
         s_n = self.signal_to_noise(target_counts, sky_counts_pp, dit, n_dit)
         s_n -= limit_sn
@@ -189,24 +267,37 @@ class HawkiEtc():
 
     def mag_for_sn(self, s_n: float,
                    dit: Quantity[u.s], n_dit: int) -> Quantity[u.mag]:
+        """
+        Calculate limiting magnitude from given S/N ratio and integration time.
+
+        Result is reached iteratively via solver. This may not always converge.
+        Outout limiting magnitude is given in Vega magnitudes.
+
+        Parameters
+        ----------
+        s_n : float
+            Input signal-to-noise ratio (dimensionless), must be > 0.0.
+        dit : astropy.Quantity
+            Detector integration time in seconds, must be >= 10 s for non-
+            destructive reading (NDR) as per the official ESO HAWK-I
+            instrument description.
+        n_dit : int
+            Number of integrations, must be >= 1.
+
+        Raises
+        ------
+        ValueError
+            Raised if `s_n` is outside the allowed range.
+
+        Returns
+        -------
+        mag : astropy.Quantity
+            Output limiting (Vega) magnitude.
+
+        """
+        if s_n < 0:
+            raise ValueError("S/N ratio must be greater than 0.0.")
+
         mag = fsolve(self._sn_for_mag_for_root, 20, (dit, n_dit, s_n))[0]
         mag = mag << u.mag
         return mag
-
-
-if __name__ == "__main__":
-    dit = 60 * u.s  # in seconds, needs Quantity
-    n_dit = 60  # simple integer
-    assert dit * n_dit == 1 * u.h  # total observing time should be 1 h
-
-    calc = HawkiEtc()  # create ETC instance
-
-    # Calculate S/N ratio from given magnitude:
-    lim_mag = 21*u.mag  # only Vega mags currently fully supported!
-    sn_ratio = calc.sn_for_mag(lim_mag, dit, n_dit)
-    print(f"S/N: {sn_ratio:.2f}")  # formatted printing
-
-    # Calculate limiting magnitude from given S/N ratio:
-    sn_ratio = 5.0  # dimensionless
-    lim_mag = calc.mag_for_sn(sn_ratio, dit, n_dit)
-    print(f"{lim_mag:.2f}")  # formatted printing
